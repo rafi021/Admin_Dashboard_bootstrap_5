17a891eac0033f3e6b28a62c0fe080ef2c82f694
rafi021 <mahmud.ibrahim021@gmail.com>
2022-06-04 09:44:57 +0600

    side menu bar

---
+++ assets/js/custom.js
@@ -0,0 +1,7 @@
+var btn = document.querySelector('.btn-slide');
+
+btn.addEventListener('click', function(){
+    document.querySelector('.side-nav').classList.toggle('slide');
+    document.querySelector('.open').classList.toggle('d-none');
+    document.querySelector('.close').classList.toggle('d-none');
+})
